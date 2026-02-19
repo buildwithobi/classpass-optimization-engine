2073c8fdde2e27f83c1dee811a24a531d64ce5af
obi <obiaanyanwu@outlook.com>
2026-02-19 05:28:34 -0800

    Updated preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,30 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 # one-hot encoding for categorical variables
 
-def preprocess_data(df):
+def preprocess_features(df, target = None):
+   
+    # copyying to avoid modifying original
     df = df.copy()
 
-    df = pd.get_dummies(df, columns = ["persona", "class_type"], drop_first = True)
+    # one-hot encoding for categorical variables
+    encoder = OneHotEncoder(drop = "first", sparse = False)
+    class_encoded = encoder.fit_transform(df[["class_type"]])
+    class_df = pd.DataFrame(class_encoded, columns = encoder.get_feature_names_out(["class_type"]))
+    df = pd.concat([df.drop("class_type", axis = 1), class_df], axis = 1)
 
-    return df
+    #scale numerical features
+    numeric_cols = ["credit_price", "current_streak", "social_score", "days_since_last_booking"]
+    scaler = StandardScaler()
+    df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+
+    #split target
+    if target:
+        y = df[target]
+        X = df.drop(columns = [target])
+        return X, y
+    else:
+        return df
 
 
