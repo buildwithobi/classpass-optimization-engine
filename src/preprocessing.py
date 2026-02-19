3ff94130d28f92a1ff9b2820a6e55af208ad9861
obi <obiaanyanwu@outlook.com>
2026-02-19 04:24:21 -0800

    Revised exploratory data analysis notebook and changed color scheme on plots and graphs

---
+++ src/preprocessing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# one-hot encoding for categorical variables
+
+def preprocess_data(df):
+    df = df.copy()
+
+    df = pd.get_dummies(df, columns = ["persona", "class_type"], drop_first = True)
+
+    return df
